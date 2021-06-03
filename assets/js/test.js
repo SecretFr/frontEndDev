a79fb1fe3c8773bdddeb4d066b23b637df943da7
DaRamG <linkclean@naver.com>
2021-06-03 16:26:26 +0900

    JavaScript
    날짜 표시 및 동적으로 색깔 변경

---
+++ assets/js/test.js
@@ -0,0 +1,39 @@
+function startTime(){
+    
+    var data = new Date()
+    var h = data.getHours();
+    var m = data.getMinutes();
+    var s = data.getSeconds();
+
+    m = minCheck(m);
+    s = minCheck(s);
+
+    console.log(h,m,s);
+
+    if(h>11){
+        document.getElementById("time").innerHTML = "PM " + h + ":" + m + ":" + s;
+        if(s>30){
+            document.getElementById("time").style.color = "aquamarine";
+            document.getElementById("time").style.backgroundColor = "black";
+        }
+        else{
+            document.getElementById("time").style.color = "hotpink";
+            document.getElementById("time").style.backgroundColor = "aqua";
+        }
+    }
+    else{
+        document.getElementById("time").innerHTML = "AM " + h + ":" + m + ":" + s;
+        if(s>30){
+            document.getElementById("time").style.color = "aquamarine";
+        }
+        else{
+            document.getElementById("time").style.color = "hotpink";
+        }
+    }
+    setTimeout(startTime, 1000);
+
+}
+function minCheck(i){
+    if(i<10) {i = "0" + i};
+    return i;
+}
